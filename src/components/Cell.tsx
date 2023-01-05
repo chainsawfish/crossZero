a07dbfa2bf56d4dbb4b579a05fa4f5071b1be30e
Denis <chainsawfish@gmail.com>
2023-01-05 17:33:14 +0700

    feat add for cell method

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Cell() {
+type CellType = {
+  value: string,
+  key: number,
+}
+
+export default function Cell(props: CellType) {
+  const handleClickCell = () => {
+
+  }
   return (
-    <div className='w-40 h-40 bg-emerald-300 shadow-md p-4 m-2'>Cell</div>
+    <div className='w-40 h-40 bg-emerald-300 shadow-md p-4 m-2'>{props.value}</div>
   )
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,12 +5,20 @@ import { cellsGrid } from '../data/cellsGrid'
 export default function Table() {
 
   const makeTable = (grid: string[][]) => {
-    for (let i = )
+    let key = 0
+    for (let i = 0; i < grid.length; i++ ){
+      for (let j = 0; j < grid[i].length; j++) {
+        key+=1
+        return (
+            <Cell value={grid[i][j]} key={key}/>
+        )
+      }
+    }
   }
 
   return (
     <div>
-        
+
     </div>
   )
 }
